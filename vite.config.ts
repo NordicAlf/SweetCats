f9674cafa5dd739bc438ff56ec77fd86d530530d
NordDev <sver4ok@protonmail.com>
2023-06-02 14:58:08 +0300

    project init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+export default defineConfig({
+  plugins: [
+    wasm(),
+    react()
+  ],
+  optimizeDeps: {
+    exclude: ['@babylonjs/havok'],
+  }
+});
