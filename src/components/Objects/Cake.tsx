a3941eaad1f597284eb5165ffd40c1592a499a43
Andrei <47153340+NordicAlf@users.noreply.github.com>
2023-11-02 00:49:29 +0300

    SCF-14 counter for cakes (#11)
    Co-authored-by: Andrei_Tsvyrko <alf.nordic@gmail.com>

--- src/components/Objects/Cake.tsx
+++ src/components/Objects/Cake.tsx
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import ObjectNames from '../../utils/constants/ObjectNames';
 import { ObjectInterface } from './Interface/ObjectInterface';
 import {useGameStore} from "../../store/GameStore";
+import usePlayerStore from "~/store/PlayerStore";
 
 export const Cake = (props: ObjectInterface) => {
   const [isVisible, setVisible] = useState(true);
   const cakeRemove = useGameStore((gameStore) => gameStore.actions.objectRemove);
+  const eatCake = usePlayerStore((playerStore) => playerStore.actions.eatCake);
 
   props.position.setY(props.position.y + 0.05);
 
@@ -17,7 +19,8 @@ export const Cake = (props: ObjectInterface) => {
       key={props.index}
       onCollisionEnter={() => {
         setVisible(false);
-        cakeRemove(props.index)
+        cakeRemove(props.index);
+        eatCake();
       }}
       name={ObjectNames.cake}
       scale={2}
